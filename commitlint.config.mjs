d97bf27601c67b3a745ef01b6c32f5c4549b61ec
Enno Gotthold <enno.gotthold@suse.com>
2026-07-17 09:01:19 +0200

    ci: add commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [2, "always", 120],
+    "footer-max-line-length": [2, "always", 120],
+    "header-max-length": [2, "always", 72],
+  },
+};
